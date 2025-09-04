98b6128626d427cc810f3dbf8a7bbd966c25945d
Aleksi O'Connor <aleksi@sharetribe.com>
2025-09-04 16:56:13 +0300

    fix breaking build

--- content/template/_meta.js
+++ content/template/_meta.js
@@ -1,1 +1,54 @@
-export default {};
+export default {
+  // Introduction
+  introduction: "Introduction",
+
+  // Configuration and Environment Variables
+  configuration: "Configuration",
+
+  // Content Management
+  "content-management": "Content Management",
+
+  // Availability management
+  availability: "Availability Management",
+
+  // Styling
+  styling: "Styling",
+
+  // Routing
+  routing: "Routing",
+
+  // Data flow and Redux
+  "state-management": "State Management",
+
+  // Maps
+  maps: "Maps",
+
+  // Security
+  security: "Security",
+
+  // Testing
+  testing: "Testing",
+
+  // Hosting
+  hosting: "Hosting",
+
+  // Analytics
+  analytics: "Analytics",
+
+  // Legacy
+  legacy: "Legacy",
+};
+
+// Introduction
+// Configuration and Environment Variables
+// Content Management
+// Availability management
+// Styling
+// Routing
+// Data flow and Redux
+// Maps
+// Security
+// Testing
+// Hosting
+// Analytics
+// Legacy
