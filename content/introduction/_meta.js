90d3a136cb11aec9ee0715f5acb6bd92a9976a55
Sari Saariaho <sari@sharetribe.com>
2025-12-10 12:15:20 +0200

    Payments: Rework Set up Stripe article

--- content/introduction/_meta.js
+++ content/introduction/_meta.js
@@ -2,6 +2,7 @@ export default {
   'introduction-to-customizing': {},
   'introducing-template': {},
   'getting-started-with-web-template': {},
+  'set-up-and-use-stripe': {},
   'development-skills': {},
   'getting-started-with-sdks': {},
   'getting-started-with-sharetribe-cli': {},
