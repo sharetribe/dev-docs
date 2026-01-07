a98307330aeb1ec33e440db259e599102fd6e9f1
Amanda Kauppinen <amanda@sharetribe.com>
2026-01-07 20:03:31 +0200

    Added community contributions page

---
+++ content/community-contributions/_meta.js
@@ -0,0 +1,12 @@
+export default {
+    index: {
+        title: 'Community contributions',
+        "theme": {
+            "typesetting": "article",
+            "pagination": false,
+            "sidebar": false,
+            "toc": false,
+            "breadcrumb": false
+        }
+    },
+}
