46b56434e6bc600d42897202115ccf8ac4a95b8b
Sari Saariaho <sari@sharetribe.com>
2026-02-04 13:45:54 +0200

    Add tracking consent params to kapa widget

--- app/layout.jsx
+++ app/layout.jsx
@@ -86,10 +86,15 @@ const kapaScript = (
     data-modal-title="Sharetribe AI Assistant"
     data-modal-example-questions-title="Try asking me..."
     data-modal-disclaimer="This **AI assistant answers Sharetribe questions** using our [documentation](https://www.sharetribe.com/docs/), [API reference](https://www.sharetribe.com/api-reference/), [help center](https://www.sharetribe.com/help/en/), [blogs](https://www.sharetribe.com/developer-blog/), [SDK reference](https://sharetribe.github.io/flex-sdk-js/) and [github files](https://github.com/sharetribe/web-template/)."
-    data-modal-example-questions="How do I customize transaction flow?,How do I change commission logic?,How to extend the web template?,How do I migrate data to Flex?"
     data-button-text-color="#FFFFFF"
     data-modal-header-bg-color="#181616"
     data-modal-title-color="#FFFFFF"
+    // Tracking consent handling
+    data-consent-required="true"
+    data-consent-screen-title="Using our AI assistant requires anonymous user tracking"
+    data-consent-screen-disclaimer="By clicking 'Allow tracking and use AI assistant', you consent to anonymous user tracking which helps us improve our service. We don't collect any personally identifiable information."
+    data-consent-screen-accept-button-text="Allow tracking and use AI assistant"
+    data-consent-screen-reject-button-text="No, thanks"
   ></script>
 );
 
