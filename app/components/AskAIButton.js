8f2eda8eb5f8e37849df209b48deeeafd4d275e5
Aleksi O'Connor <aleksi@sharetribe.com>
2026-02-11 11:57:02 +0100

    add ai CTA in topbar

---
+++ app/components/AskAIButton.js
@@ -0,0 +1,23 @@
+'use client';
+
+export default function AskAIButton() {
+  return (
+    <button
+      onClick={() => window.Kapa?.open()}
+      className="ask-ai-button"
+      type="button"
+    >
+      Ask AI
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        style={{ marginLeft: '4px' }}
+      >
+        <path d="M16 18a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm0 -12a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm-7 12a6 6 0 0 1 6 -6a6 6 0 0 1 -6 -6a6 6 0 0 1 -6 6a6 6 0 0 1 6 6z" />
+      </svg>
+    </button>
+  );
+}
