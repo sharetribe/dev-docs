23054f2dd61d70be2aea07d47d5b431cbb25e1bc
Sari Saariaho <sari@sharetribe.com>
2026-02-12 09:28:43 +0200

    Update documentation to include information about transaction fields

--- content/how-to/transaction-process/_meta.js
+++ content/how-to/transaction-process/_meta.js
@@ -2,4 +2,5 @@ export default {
   'create-new-transaction-process-with-cli': {},
   'edit-transaction-process-with-sharetribe-cli': {},
   'change-transaction-process-in-template': {},
+  'add-transaction-fields': {},
 };
