8ffaaad7267194d31b110e4a20c0f66e0fabc7d2
Aleksi O'Connor <aleksi@sharetribe.com>
2026-03-17 11:30:32 +0200

    add a copy button to docs

--- app/[[...mdxPath]]/page.jsx
+++ app/[[...mdxPath]]/page.jsx
@@ -1,15 +1,15 @@
-import { generateStaticParamsFor, importPage } from "nextra/pages";
-import { useMDXComponents as getMDXComponents } from "../../mdx-components";
+import { generateStaticParamsFor, importPage } from 'nextra/pages';
+import { useMDXComponents as getMDXComponents } from '../../mdx-components';
 
-export const generateStaticParams = generateStaticParamsFor("mdxPath");
+export const generateStaticParams = generateStaticParamsFor('mdxPath');
 
 export async function generateMetadata(props) {
   const params = await props.params;
   const { metadata } = await importPage(params.mdxPath);
 
   // Generate canonical URL
-  const baseUrl = "https://www.sharetribe.com/docs";
-  const canonicalPath = params.mdxPath ? params.mdxPath.join("/") : "";
+  const baseUrl = 'https://www.sharetribe.com/docs';
+  const canonicalPath = params.mdxPath ? params.mdxPath.join('/') : '';
   const canonicalUrl = `${baseUrl}/${canonicalPath}`;
 
   return {
@@ -27,12 +27,12 @@ export default async function Page(props) {
   const result = await importPage(params.mdxPath);
   const { default: MDXContent, toc, metadata, sourceCode } = result;
   // if we define hideCopyPage = true in front matter, we can disable the llm copy page feature for a specific page
-  // const { hideCopyPage } = metadata;
+  const { hideCopyPage } = metadata;
   return (
     <Wrapper
       toc={toc}
       metadata={metadata}
-      // sourceCode={hideCopyPage ? "" : sourceCode}
+      sourceCode={hideCopyPage ? '' : sourceCode}
     >
       <MDXContent {...props} params={params} />
     </Wrapper>
